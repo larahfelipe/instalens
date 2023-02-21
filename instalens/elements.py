160f4a09091908c7edef8e24235ac614cc72170d
Felipe Lara <felipeahlara@gmail.com>
2023-02-21 16:30:52 -0300

    Refactor and Feat: Browser and working on sign out function

--- instalens/elements.py
+++ instalens/elements.py
@@ -1,10 +1,14 @@
 class Elements:
-  INPUT_USERNAME = 'username'
+  INPUT_AUTH_USERNAME = 'username'
 
-  INPUT_PASSWORD = 'password'
+  INPUT_AUTH_PASSWORD = 'password'
 
-  INPUT_MFA = 'verificationCode'
+  INPUT_AUTH_MFA = 'verificationCode'
 
-  DIALOG_FOLLOWING = 'div[role="dialog"] div div[role="dialog"] div[role="tablist"] + div div div:nth-child({}) a.notranslate'
+  DIALOG_ACCOUNT_FOLLOWING = 'div[role="dialog"] div div[role="dialog"] div[role="tablist"] + div div div:nth-child({}) a.notranslate'
 
-  DIALOG_FOLLOWERS = 'div[role="dialog"] div div[role="dialog"] div div:nth-child(2) div div:nth-child({}) a.notranslate'
+  DIALOG_ACCOUNT_FOLLOWERS = 'div[role="dialog"] div div[role="dialog"] div div:nth-child(2) div div:nth-child({}) a.notranslate'
+
+  BTN_ACCOUNT_SETTINGS = 'main[role="main"] header section div:nth-child(3) button[@type="button"]'
+
+  DIALOG_ACCOUNT_SETTINGS_BTN_LOGOUT = 'Log Out'
